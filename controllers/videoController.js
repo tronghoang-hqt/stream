184c115e6a0800852782c6353c0a3e549bffd680
tronghoang <tronghoang2403@gmail.com>
2020-03-20 16:13:06 +0700

    no message

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,14 @@
 var videoService = require('../services/videoService');
+
 module.exports = {
     getListVideo: function(req, res, next) {
-        videoService.getListVideoService().then(videos => {
-            res.send({ 'data': videos });
+        videoService.getListVideoService().then(async data => {
+            await data.forEach(async videos => {
+                await videos.videos.forEach(video => {
+                    video.sources = [video.sources];
+                })
+            });
+            await res.send({ "data": data })
         });
-
     }
 }
